6daa270d4b389803bda5c6bd083248ad415180a1
bayaraapurev <bayaraapurev@gmail.com>
2026-09-10 17:13:26 +0800

    zarim aldaag zassan

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Script from "next/script"; // 1. Script-ийг импорт хийх
+import Script from "next/script"; // Импорт хэвээрээ байна
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700', '900'],
@@ -17,20 +17,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="mn">
-      {/* 2. Энд <head> хэсгийг нэмж Google скрипт болон meta кодоо байрлуулна */}
       <head>
-        {/* Google Search Console баталгаажуулах мета код (Хэрэв байгаа бол) */}
         <meta name="google-site-verification" content="googlea330e799e52e5834" />
 
-        {/* Google Analytics скрипт */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-333RDHZXQZ"></script>
-        <script>
+        {/* Google Analytics скриптийг зөвөөр Next.js Script ашиглан бичих */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-333RDHZXQZ"
+        />
+        <Script id="google-analytics">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-333RDHZXQZ');
-        </script>
+          `}
+        </Script>
       </head>
 
       <body className={`${roboto.className} antialiased flex flex-col min-h-screen bg-gray-50`}>
